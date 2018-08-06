55e8acf4a366a7c062f2bed1f194f397e3081dca
Ross Jennings <themissinglinq@gmail.com>
2018-08-05 20:48:17 -0500

    failing test I'll deal with tomorrow, I guess I broke left() when I fixed left join

--- test/mssql/kitchen_sink.sql
+++ test/mssql/kitchen_sink.sql
@@ -41,7 +41,7 @@ with cte as (select 1)
 select sum(s.num) as [sum]
 from something s
 left join dbo.other
-  on dbo.other.foo = @foo
+  on left(dbo.other.foo, 1) = right(@foo, 1)
 left join dbo.table_valued_func(@foo) as e
   on s.asdf = zzz.the_func(e)
 where s.number in (1, 2, 3)
